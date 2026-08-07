a8e6dd6a45f5c8a076134099edcef61cdfd08181
Abtin <159820257+abtin-rz@users.noreply.github.com>
2026-08-07 17:05:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ add_configs(extract_br(URL_BR))
 # نوشتن فایل از ابتدا
 with open(OUTPUT_FILE, "w", encoding="utf-8") as file:
 
-    file.write("vless:....#Collected%20by%20Abtin\n")
-    file.write("vless:....#GitHub:%20github.com/abtin-rz/telegram-scraper\n")
+    file.write("vless://....#Collected%20by%20Abtin\n")
+    file.write("vless://....#GitHub:%20github.com/abtin-rz/telegram-scraper\n")
 
     for config in sorted(configs):
         file.write(config + "\n")
