3a9fe529d4bd082df9b54a126cfdfe86a3d7d77d
abtin-rz <abtinrezaie@gmail.com>
2026-08-07 16:37:53 +0330

    add name in the first of soup file

--- main.py
+++ main.py
@@ -100,6 +100,8 @@ add_configs(extract_br(URL_BR))
 # نوشتن فایل از ابتدا
 with open(OUTPUT_FILE, "w", encoding="utf-8") as file:
 
+    file.write("# Collected by Abtin\n")
+    file.write("# GitHub: https://github.com/abtin-rz/telegram-scraper\n\n")
+
     for config in sorted(configs):
-        file.write(config)
-        file.write("\n\n")
+        file.write(config + "\n")
